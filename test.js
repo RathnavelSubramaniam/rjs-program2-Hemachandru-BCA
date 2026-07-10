aa117048e39ebac69f4b0772bbe537377e18a368
Hemachandru <2006hemachandru@gmail.com>
2026-07-10 18:55:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-test("uses the required template literal", () => {
+test("✅ uses the required template literal", () => {
   const code = fs.readFileSync("greet.js", "utf8");
 
   expect(code).toMatch(
